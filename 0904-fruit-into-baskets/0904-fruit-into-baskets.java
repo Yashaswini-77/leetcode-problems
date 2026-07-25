e8feffb59251d1f87469afdc91e194507adb6a3e
Yashaswini R <yashaswiniy502@gmail.com>
2026-07-25 12:52:40 +0530

    Time: 59 ms (13.04%), Space: 70.8 MB (17.82%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -3,8 +3,8 @@ class Solution {
         HashMap<Integer,Integer>map=new HashMap<>();
         int left=0;
         int max=0;
-        for(int right=0;right<fruits.length;right++){
-        map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+        for(int i=0;i<fruits.length;i++){
+        map.put(fruits[i],map.getOrDefault(fruits[i],0)+1);
         while(map.size()>2){
             map.put(fruits[left],map.get(fruits[left])-1);
             if(map.get(fruits[left])==0){
@@ -12,7 +12,7 @@ class Solution {
             }
                 left++;
             }
-            max=Math.max(max,right-left+1);
+            max=Math.max(max,i-left+1);
         }    
         return max;
     }
